734c6291bb0c32e4b5d628685d1b2f85c6ec061e
Simon Lundström <simmel@soy.se>
2011-01-16 16:17:01 +0100

    Make sure the track is available. Use Maxmind's JS API to find out which country the user is from and make sure that we only add the track if the user can listen to it. Also make sure we only add five tracks. This is going to be customizeable in the future.

--- app.js
+++ app.js
@@ -33,13 +33,23 @@ function fetch(/* object */ request, /* function */ respond)
       { q: 'artist:"' + artist + '"' },
       function (xml) {
         var a = $('textarea')
+        var user_country = geoip_country_code();
+        var tracks_added = 0;
         $('track', xml).each(function(i) {
-          if (i > 5) {
+          if (tracks_added >= 5) {
             return;
           }
           var track = $(this).attr('href')
-          console.log("Adding track " + track);
-          a.val(a.val() + track + "\n");
+          track_territory = $(this).find('territories:contains(' + user_country + ')').first();
+          // Only add track if it's available "worldwide" or in the users country
+          if ($(this).find('territories').first().text() == "worldwide" || track_territory.length) {
+            console.log("Adding track " + track);
+            a.val(a.val() + track + "\n");
+            tracks_added++;
+          }
+          else {
+            console.log("Not adding " + track + " since it's not available in user territory (" + user_country + ").");
+          }
         });
       }
     );
