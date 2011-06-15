fdbeafb4083282c6ae3c03e2847ac9abff9951e4
Simon Lundström <simmel@soy.se>
2011-06-15 20:37:18 +0200

    Commit early, commit often...
    * Add support for writing and pasting multiple artists separating them
    with a comma and they automatically become a new <input>.
    * Add the possibility to search tracks from artists of an event on
    Last.fm.
    * Add some style and a "logo".

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ if (!("console" in window)) {
 jQuery(function ($) {
 
   function fetch(/* object */ request, /* function */ respond) {
+    if ($('input[name=search_type]:checked').attr("id") != "artist") {
+      return;
+    }
+
     console.log( request.term );
 
     $.get(
@@ -72,6 +76,80 @@ jQuery(function ($) {
     );
   }
 
+  function set_submit_action() {
+    $('form').unbind('submit');
+    switch ($(this).attr("id")) {
+      case "artist": 
+        $('form').submit(fetch_tracks_of_artists);
+        break;
+      case "event":
+        $('form').submit(fetch_tracks_from_event);
+        break;
+      default:
+        alert("fubar");
+    }
+  }
+
+  function fetch_tracks_of_artists() {
+    if (document.activeElement) {
+      $(document.activeElement).autocomplete("close");
+    }
+
+    var artists = [];
+    $('form input[type=search]').each(function() {
+        artists.push($(this).val());
+    });
+
+    fetch_tracks_from_artists(artists);
+
+    return false;
+  }
+
+  function fetch_tracks_from_artists(artists) {
+    $('textarea').val("");
+
+    var number_of_tracks = parseInt($('#number_of_tracks').val(), 10);
+    if (isNaN(number_of_tracks)) {
+      number_of_tracks = 5;
+    }
+
+    jQuery.each(artists, function(k,artist) {
+      console.log("Found artist: " + artist);
+      fetch_tracks(artist, number_of_tracks, add_to_textarea);
+    });
+  }
+
+  function fetch_artists_from_event(data, status) {
+    var artists = [];
+    $(data).find("artist").each(function() {
+        artists.push($(this).text());
+    });
+
+    fetch_tracks_from_artists(artists);
+  }
+
+  function fetch_tracks_from_event() {
+    $('form input[type=search]').each(function() {
+      var event_url = $(this).val();
+      var event_id = null
+      if (event_url.match(/\/(?:festival|event)\/(\d+?)\+/)) {
+        console.log("Found event: " + event_url);
+        event_id = RegExp.$1;
+      }
+      else {
+        console.log("Couldn't find the event ID in your URL. The URLs should look like this:\nhttp://www.last.fm/festival/1834310+Dans+Dakar\nor this:\nhttp://www.last.fm/event/1821078+DJ+Shadow");
+        return;
+      }
+
+      $.get(
+        'fetch_event.lua',
+        { event: event_id },
+        fetch_artists_from_event
+      );
+    });
+    return false;
+  }
+
   $('.searcher').live('focus', function() {
     $(this).autocomplete({
       source: fetch,
@@ -79,10 +157,53 @@ jQuery(function ($) {
     });
   });
 
+  // Set submit action when radio button is pressed
+  $('input[name=search_type]').click(set_submit_action);
+  // Set submit action when site is loaded
+  $('input[name=search_type]').each(set_submit_action);
+
+  $('.searcher').live('paste',function() {
+    var e = $(this);
+    setTimeout(function() {
+      var pasted = $(e).val();
+      if (! /,/.test(pasted)) {
+        console.log("Something was pasted, but there was no comma.");
+        return;
+      }
+      var artists = [];
+
+      artists = pasted.split(/ ?, ?/).reverse();
+
+      // Replace where we pasted with the first artist
+      // FIXME: This makes autocomplete trigger. Stop it!
+      $(e).val(artists.pop());
+
+      var last = e;
+      jQuery.each(artists.reverse(), function(k, artist) {
+        last = insert_new_search_input_last(artist, last);
+      });
+    },
+    // http://stackoverflow.com/questions/686995/jquery-catch-paste-input/3553217#3553217
+    0);
+  });
+
+  function insert_new_search_input_last(value, element) {
+    return $('input[type=search]').first().clone().val(value).insertAfter(element).focus();
+  }
+
   $('.searcher').live('keydown',function(e) { 
+    var comma_is_last = /,$/;
+    if (e.which == $.ui.keyCode.SPACE && comma_is_last.test($(this).val())) {
+      $(this).val($(this).val().replace(comma_is_last, ""));
+      insert_new_search_input_last("", $(this));
+      $(this).autocomplete("close");
+      // We don't want the space to be inserted
+      e.preventDefault();
+    }
+
     // Only create new input if old input is empty
     if (e.shiftKey && e.which == $.ui.keyCode.ENTER && $(this).val() != '') {
-      $('input[type=search]').first().clone().val("").insertAfter($(this)).focus();
+      insert_new_search_input_last("", $(this));
       $(this).autocomplete("close");
       // Needed or else we will submit
       return false;
@@ -97,26 +218,6 @@ jQuery(function ($) {
       $(this).remove();
       $('input[type=search]').last().focus();
     }
-  });
-
-  $('form').submit(function() {
-    $('textarea').val("");
-
-    if (document.activeElement) {
-      $(document.activeElement).autocomplete("close");
-    }
-    var number_of_tracks = parseInt($('#number_of_tracks').val(), 10);
-    if (isNaN(number_of_tracks)) {
-      number_of_tracks = 5;
-    }
-    $('form input[type=search]').each(function() {
-      var artist = $(this).val();
-      if (artist) {
-        console.log("Found artist: " + artist);
-        fetch_tracks(artist, number_of_tracks, add_to_textarea);
-      }
-    });
-    return false;
   });
 
   $('.searcher').first().focus();
